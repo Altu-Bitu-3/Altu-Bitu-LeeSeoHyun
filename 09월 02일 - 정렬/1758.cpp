ee03b2504bddc6c9d04bacd3b7e6802b6e33bb0a
leeseohyun <applemint2003@gmail.com>
2022-09-02 04:16:08 +0900

    [정렬] 9월 2일

--- 09월 02일 - 정렬/1758.cpp
+++ 09월 02일 - 정렬/1758.cpp
@@ -4,15 +4,19 @@ using namespace std;
 int main()
 {
 	int N;
+	long long int MAX = 0;
 	cin >> N;
 	int* arr = new int[N];
-	for (int i = 0; i < N; i++){
+	for (int i = 0; i < N; i++) {
 		cin >> arr[i];
 	}
 	sort(arr, arr + N);
-	for (int i = 0; i < N; i++){
-		cout << arr[i] << endl;
+	for (int i = 0; i < N; i++) {
+		arr[i] = arr[i] - (N - 1 - i);
+		if (arr[i] > 0)
+			MAX += arr[i];
 	}
+	cout << MAX;
 	delete[] arr;
 	return 0;
 }
--- 09월 02일 - 정렬/2751.cpp
+++ 09월 02일 - 정렬/2751.cpp
@@ -1,25 +1,4 @@
-/*2750
-#include<iostream>
-#include<algorithm>
-using namespace std;
-int main()
-{
-	int N;
-	int arr[1000] = {};
-	cin>>N;
-	for (int i = 0; i < N; i++){
-		cin >> arr[i];
-	}
-	sort(arr, arr + N);
-	for (int i = 0; i < N; i++){
-		cout << arr[i] << endl;
-	}
-	return 0;
-}
-*/
-
-/*2751
-#include<iostream>
+include<iostream>
 #include<algorithm>
 using namespace std;
 int main()
@@ -33,29 +12,6 @@ int main()
 	sort(arr, arr + N);
 	for (int i = 0; i < N; i++){
 		cout << arr[i] << "\n";
-	}
-	delete[] arr;
-	return 0;
-}
-*/
-//10825
-#include<iostream>
-#include<algorithm>
-#include<string>
-using namespace std;
-int main()
-{
-	int N;
-	cin >> N;
-	string (*arr)[4] = new string[N][4];
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < 4; j++) {
-			cin >> arr[N][j];
-		}
-	}
-	sort(arr, arr + N, comp);
-	for (int i = 0; i < N; i++) {
-		cout << arr_name << "\n";
 	}
 	delete[] arr;
 	return 0;
