8de19ff1c7759f900008bd7383e798725ef6c84f
leeseohyun <applemint2003@gmail.com>
2022-09-11 22:33:55 +0900

    [맵과 셋] 9월 6일

--- 09월 06일 - 맵과 셋/10867.cpp
+++ 09월 06일 - 맵과 셋/10867.cpp
@@ -1,13 +1,15 @@
+//벡터로 푼 코드
+/*
 #include<iostream>
 #include<vector>
 #include<algorithm>
 using namespace std;
 
 int main() {
-	int N;
-	cin >> N;
+	int n;
+	cin >> n;
 	vector<int> arr;
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < n; i++) {
 		int a;
 		bool overlap = false;
 		cin >> a;
@@ -29,4 +31,29 @@ int main() {
 	for (int i = 0; i < arr.size(); i++) {
 		cout << arr[i] << " ";
 	}
+}*/
+
+//셋으로 푼 코드 (훨씬 간단)
+#include<iostream>
+#include<set>
+using namespace std;
+
+int main() {
+	int n, input;
+	set<int> s;
+	cin >> n;
+	while (n--) {
+		cin >> input;
+		s.insert(input);
+	}
+	//set<int>::iterator iter;
+	//auto 키워드는 초기화 값에 따라 데이터 타입을 자동으로 정해줌
+	/*
+	for(auto iter = s.begin(); iter!= s.end(); iter++)
+	cout << *iter << ' ';
+	*/
+	for (auto iter : s) {
+		cout << iter << ' ';
+	}
+	return 0;
 }
--- 09월 06일 - 맵과 셋/1620.cpp
+++ 09월 06일 - 맵과 셋/1620.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <map>
 #include <string>
+
 using namespace std;
+
 int main() {
 	ios::ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	int n, m;
 	string input;
-	map<string, int> pocket_name;
-	map<int, string> pocket_num;
+	map<string, int> pocket_name; //이름이 key, 번호가 value
+	map<int, string> pocket_num;  //번호가 key, 이름이 value
 
 	cin >> n >> m;
 	for (int i = 1; i <= n; i++) {
@@ -18,7 +20,7 @@ int main() {
 	}
 	while (m--) {
 		cin >> input;
-		if (isdigit(input[0])) {
+		if (isdigit(input[0])) { //숫자 입력이라면
 			int number = stoi(input);
 			cout << pocket_num[number] << '\n';
 		}
