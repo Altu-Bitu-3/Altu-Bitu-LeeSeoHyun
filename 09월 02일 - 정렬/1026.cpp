4dbe126969028dcbd37a85ff86b45ae77caa6bcc
leeseohyun <applemint2003@gmail.com>
2022-09-12 03:58:57 +0900

    [정렬] 9월 2일

--- 09월 02일 - 정렬/1026.cpp
+++ 09월 02일 - 정렬/1026.cpp
@@ -3,23 +3,23 @@
 using namespace std;
 int main()
 {
-	int N, MIN = 0;
-	cin >> N;
-	int* arr_A = new int[N];
-	int* arr_B = new int[N];
-	for (int i = 0; i < N; i++) {
-		cin >> arr_A[i];
+	int n, min = 0;
+	cin >> n;
+	int* arr_a = new int[n];
+	int* arr_b = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr_a[i];
 	}
-	for (int i = 0; i < N; i++) {
-		cin >> arr_B[i];
+	for (int i = 0; i < n; i++) {
+		cin >> arr_b[i];
 	}
-	sort(arr_A, arr_A + N);
-	sort(arr_B, arr_B + N);
-	for (int i = 0; i < N; i++) {
-		MIN += arr_A[i] * arr_B[N - 1 - i];
+	sort(arr_a, arr_a + n);
+	sort(arr_b, arr_b + n);
+	for (int i = 0; i < n; i++) {
+		min += arr_a[i] * arr_b[n - 1 - i];
 	}
-	cout << MIN;
-	delete[] arr_A;
-	delete[] arr_B;
+	cout << min;
+	delete[] arr_a;
+	delete[] arr_b;
 	return 0;
 }
