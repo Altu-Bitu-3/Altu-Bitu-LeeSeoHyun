8255ee71b2feddac189c7fa4660e362d9bcc07c1
leeseohyun <applemint2003@gmail.com>
2022-10-03 12:07:14 +0900

    Update 11048.cpp

--- 09월 27일 - 동적 계획법/11048.cpp
+++ 09월 27일 - 동적 계획법/11048.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int arr[1001][1001];
 
 int main() {
-	= nt n, m;
+	int n, m;
 	cin >> n >> m;
-	for (int i = 1; i <= n; i++)_{
+	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= m; j++) {
 			cin >> arr[i][j];
 		}
